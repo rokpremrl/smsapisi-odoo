6136f7b0919395dac8491695fb80470bd07b9cfc
Luka Zorko <luka.zorko@guru.si>
2024-04-17 07:35:28 +0200

    17.0.1.0.0

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "smsapisi_connector",
+    'summary': "Send SMS with SMSapi.si",
+    'author': "Guru d.o.o.",
+    'website': "https://www.guru.si/",
+    'license': 'LGPL-3',
+    'category': 'Technical',
+    'version': '17.0.1.0.0',
+    'depends': ['base', "sms", "iap_alternative_provider", "phone_validation"],
+    'external_dependencies': {
+        'python': ['phonenumbers', 'requests']
+    },
+    'data': [
+        'data/iap_account_data.xml',
+        'data/ir_cron.xml',
+        'views/iap_account.xml',
+        'views/sms_sms.xml',
+        'views/sms_resend.xml'
+    ],
+}
