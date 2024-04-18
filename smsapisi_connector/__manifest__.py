1581dfcf0de8a605ea77b7ebe31ba25daec0487b
Luka Zorko <luka.zorko@guru.si>
2024-04-18 10:34:00 +0200

    new icon, added banner

--- smsapisi_connector/__manifest__.py
+++ smsapisi_connector/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "smsapisi_connector",
+    'name': "SMSapi.si Connector",
     'summary': "Send SMS with SMSapi.si",
     'author': "Guru d.o.o.",
     'website': "https://www.guru.si/",
@@ -18,4 +18,5 @@
         'views/sms_sms.xml',
         'views/sms_resend.xml'
     ],
+    'images': ['static/description/smsapi_banner.png'],
 }
